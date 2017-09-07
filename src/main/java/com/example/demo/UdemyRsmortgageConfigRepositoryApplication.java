bd7dd757220f1a277ec5f9452bb1c86732d10f2b
juulyy <bienvenu.julia@gmail.com>
2017-09-07 15:03:47 +0300

    first commit

---
+++ src/main/java/com/example/demo/UdemyRsmortgageConfigRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyRsmortgageConfigRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyRsmortgageConfigRepositoryApplication.class, args);
+	}
+}
